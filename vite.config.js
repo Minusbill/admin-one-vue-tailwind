987d221843ba58a17be765a9fbfa9cd70a8b81b9
bill jian <minus502@126.com>
2023-06-20 15:47:43 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/billjian-bot/",
+  // base: "/billjian-bot/",
   plugins: [vue()],
   resolve: {
     alias: {
